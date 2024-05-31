302e41adc59a59863301a048b6be32ad42e5d5c0
Robin Heathcote <robin.heathcote@icloud.com>
2024-05-31 09:46:28 +0100

    Initial Commit

---
+++ Yonder Watch App/YonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonderApp.swift
+//  Yonder Watch App
+//
+//  Created by Robin Heathcote on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Yonder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Yonder/YonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonderApp.swift
+//  Yonder
+//
+//  Created by Robin Heathcote on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct YonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
